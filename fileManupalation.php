b23b5e0e6532b97127d069c6f3e9cdc9e01a1b68
Junior Elephant <szmoaj100@gmail.com>
2025-07-15 18:21:06 +0600

    last class of php

--- fileManupalation.php
+++ fileManupalation.php
@@ -1,5 +1,25 @@
 <?php
-// $var = readfile('readme.text');
+ $var = 'readme.text';
 // echo $var;
 
 // file exist mone rakhte hobe
+// unlink mone rakhte hobe
+// read write aphend
+
+$content = 'hi this is another file';
+
+if(is_writable($var)){
+    if(!$handle = fopen($var,'a')){
+        echo "cannot open this file";
+        exit;
+    }
+    if(!fwrite($handle, $content)){
+        echo "can not write this file";
+        exit;
+    }
+    echo "wrote the content";
+    fclose($handle);
+}
+else{
+    echo 'not writable';
+}
